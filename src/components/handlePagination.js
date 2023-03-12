407bb6184f3331d17afc93861bd917dcd819dc26
wyenelle <favourkcb@gmail.com>
2023-03-12 02:29:01 +0100

    .

--- src/components/handlePagination.js
+++ src/components/handlePagination.js
@@ -7,22 +7,25 @@ export default function handlePagination(arr){
 
     const perPage = 2
     
-    const paginatedData = computed(() => arr.slice((page.value - 1) * perPage, page.value * perPage))
+    const paginatedData = computed(() => data.slice((page.value - 1) * perPage, page.value * perPage))
 
     const nextPage = () => {
         if(page.value !== Math.ceil(data.length / perPage)) {
             page.value += 1
         }
+        console.log(page.value)
     }
 
     const backPage = () => {
          if(page.value !== 1){
             page.value -= 1
          }
+         console.log(page.value)
     }
 
     const goToPage = (numPage) => {
         page.value = numPage
+        console.log(page.value)
     }
     return {data,paginatedData,perPage,page,nextPage,backPage,goToPage}
 }
