bcf7d54345a86030f05c22e20142c92461d3762e
wyenelle <favourkcb@gmail.com>
2023-03-12 18:30:08 +0100

    reverted to former code

--- src/components/PageNumbers.vue
+++ src/components/PageNumbers.vue
@@ -1,28 +1,26 @@
 <script setup>
-import handlePagination from './handlePagination.js';
+const emits = defineEmits(['number-change'])
+const {arr,arrLength,postPerPage} = defineProps(['arr','arrLength','postPerPage'])
 
-const {arr} = defineProps({
-  arr:Array
-})
-
-const {data,perPage,nextPage,backPage,goToPage} = handlePagination(arr)
+console.log(arrLength,postPerPage)
 
 let number_of_pages = []
-for(let i = 1; i <= Math.ceil(data.length / perPage);i++){
+for(let i = 1; i <= Math.ceil(arrLength / postPerPage);i++){
     number_of_pages.push(i)
 }
 </script>
 
 <template>
+    
     <div class="flex-normal px-2 h-10 py-3 mt-5 ">
-        <button class="border border-slate-200 rounded-md p-2 font-bold " @click="backPage">
+        <!-- <button class="border border-slate-200 rounded-md p-2 font-bold " @click="backPage">
         prev
-    </button>
+    </button> -->
     <ul  class=" flex-normal gap-2 px-3">
-        <li @click="goToPage(num)" v-for="num in number_of_pages" :key="num" class="border border-slate-200 rounded-md p-2 font-bold dark:text-sky-400"> {{ num }}</li>
+        <li @click="emits('number-change', num)" v-for="num in number_of_pages" :key="num" class="border border-slate-200 rounded-md p-2 font-bold dark:text-sky-400"> {{ num }}</li>
     </ul>
-    <button class="border border-slate-200 rounded-md p-2 font-bold " @click="nextPage">
+    <!-- <button class="border border-slate-200 rounded-md p-2 font-bold " @click="nextPage">
         next
-    </button>
+    </button> -->
     </div>
 </template>
--- src/components/Repositories.vue
+++ src/components/Repositories.vue
@@ -9,35 +9,34 @@ const {arr} = defineProps({
   arr:Array
 })
 
-const {paginatedData} = handlePagination(arr)
 
-// const arrLength = arr.length
-// const postPerPage = 2
-// let currentPage = ref(1)
+const arrLength = arr.length
+const postPerPage = 2
+const currentPage = ref(1)
 
-// const route = useRouter()
-  // const goToDetails = () => {
-  //   route.push(`/repo/${id}`)
-  // }
+const route = useRouter()
+  const goToDetails = () => {
+    route.push(`/repo/${id}`)
+  }
 
 // Getting the index first and last values of each page dynamicaaly
-// let index_of_last_post = currentPage.value * postPerPage
-// const index_of_first_post = index_of_last_post - postPerPage
-
-// Alter the current page
-// const changePage = (number) => {
-//     currentPage.value = number
-//      console.log(currentPage.value);
-//     }
+let index_of_last_post = currentPage.value * postPerPage
+const index_of_first_post = index_of_last_post - postPerPage
 
 // Creating an array of the first to last index for each page 
-// const currentPost = arr.slice(index_of_first_post, index_of_last_post)
+const currentPost = arr.slice(index_of_first_post, index_of_last_post)
+
+// Alter the current page
+const changePage = (number) => {
+    currentPage.value = number
+    }
+
 </script>
 
 
 <template>
     <div >
-        <Info :arr="num" v-for="num in paginatedData" :key="num"  />
+        <Info  :arr="num"  v-for="num in currentPost"  :key="num"  />
     </div>
-    <PageNumbers :arr="arr" />
+    <PageNumbers @numberChange="changePage" :arr="arr" :arrLength="arrLength" :postPerPage="postPerPage"/>
 </template>
